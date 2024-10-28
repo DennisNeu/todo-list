5320e48f8bf700e6696c0149c57b108c48ab335f
Dennis <76845672+DennisNeu@users.noreply.github.com>
2024-10-28 21:56:56 +0100

    add basic CLI interface

--- main.py
+++ main.py
@@ -3,15 +3,31 @@ from todo_list import Todo_List
 
 todo_list = Todo_List()
 
-todo_list.add_task("Buy groceries")
-todo_list.add_task("Finish coding project")
+while True:
+    print("1. show tasks")
+    print("2. create task")
+    print("3. complete task")
+    print("4. delete task")
+    print("5. exit")
 
-todo_list.show_tasks()
+    choice = input("Choose an option: ")
 
-todo_list.delete_task(1)
-
-todo_list.show_tasks()
-
-todo_list.complete_task(1)
-
-todo_list.show_tasks()
+    if choice == "1":
+        todo_list.show_tasks()
+    if choice == "2":
+        description = input("please input task description: ")
+        todo_list.add_task(description)
+    if choice == "3":
+        try:
+            index = int(input("Please input number of task to be completed: "))
+            todo_list.complete_task(index)
+        except ValueError:
+            print("Please enter a valid number")
+    if choice == "4":
+        try:
+            index = int(input("Please input number of task to be deleted: "))
+            todo_list.delete_task(index)
+        except ValueError:
+            print("Please enter a valid number")
+    if choice == "5":
+        exit()
