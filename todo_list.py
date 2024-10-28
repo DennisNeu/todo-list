9af96c742fd057b26aba11c3aad4c687043b8289
Dennis <76845672+DennisNeu@users.noreply.github.com>
2024-10-28 21:20:44 +0100

    show_list now shows index

--- todo_list.py
+++ todo_list.py
@@ -9,8 +9,8 @@ class Todo_List:
         if not self.tasks:
             print("Tasks are empty")
             return
-        for task in self.tasks:
-            print(task)
+        for index, task in enumerate(self.tasks):
+            print(f"#{index + 1}: {task}")
     
     def add_task(self, description):
         task = Task(description)
